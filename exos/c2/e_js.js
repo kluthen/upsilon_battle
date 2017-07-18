14f47ce5ec2309e517d2ce5740dc46c98c5b5c11
Bastien Barcaioni <bastien.barcaioni@gmail.com>
2017-07-18 19:29:48 +0200

    Ajout des documents annexes pour le cours 2

---
+++ exos/c2/e_js.js
@@ -0,0 +1,8 @@
+/*
+ *  JS pour le cours 2 HTML - Style et Javascript. 
+ *  On entrera pas dans les details maintenant, il s'agit juste d'un exemple. 
+ */
+
+toggleBackground = function() {
+    $("body").toggleClass("blueish");
+}
