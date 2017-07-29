71a9ada4a5fee4ddb200249f242586bd5e6df031
Bastien Barcaioni <bastien.barcaioni@gmail.com>
2017-07-29 22:02:22 +0200

    exo 2

--- exos/c1/soy.exs
+++ exos/c1/soy.exs
@@ -4,6 +4,27 @@ aujourdhui= 1
 plustard= 3
 differance= rem(aujourdhui + plustard,7)
 
-    IO.puts "Dans #{plustard} jours, sachant que nous somme un #{Enum.at(jours, aujourdhui)} nous serons un #{Enum.at(jours, differance)} "
+IO.puts "Dans #{plustard} jours, sachant que nous somme un #{Enum.at(jours, aujourdhui)} nous serons un #{Enum.at(jours, differance)} "
 
-    
+
+largeur= [1,2,3,4,5,6,7,8,9,10]
+hauteur= [1,2,3,4,5,6,7,8,9,10]
+
+resultat = for ligne <- hauteur do
+    for colone <- largeur do
+        case ligne do
+            1 ->"x"
+            10 -> "x"
+            _   ->
+                
+                case colone do 
+                    1 ->"x"
+                    10 ->"x"
+                    _  ->"o"
+                end
+        end 
+    end
+end
+
+
+IO.inspect resultat
